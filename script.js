01f04416057b176084d2b3deb3bf6bc684b663fb
Sujeet <singhsujeet772@gmail.com>
2024-01-10 11:50:09 +0530

    joke api includedd with fetch and async method

--- script.js
+++ script.js
@@ -85,17 +85,37 @@ const VoiceRSS = {
     },
 };
 
-function test(){
-    VoiceRSS.speech({
-        key: 'a3f70c1ee4fc45ff9a10e0f73e0c0c35',
-        src: 'Hello, world!',
-        hl: 'en-us',
-        v: 'Linda',
-        r: 0, 
-        c: 'mp3',
-        f: '44khz_16bit_stereo',
-        ssml: false
-    });
+// function test(){
+//     VoiceRSS.speech({
+//         key: 'a3f70c1ee4fc45ff9a10e0f73e0c0c35',
+//         src: 'Hello, world!',
+//         hl: 'en-us',
+//         v: 'Linda',
+//         r: 0, 
+//         c: 'mp3',
+//         f: '44khz_16bit_stereo',
+//         ssml: false
+//     });
+// }
+
+// test();
+
+//Get Jokes from Joke API
+async function getJokes(){
+    const apiUrl = 'https://v2.jokeapi.dev/joke/Programming?blacklistFlags=nsfw,religious,political,racist,sexist,explicit';
+    let joke = '';
+    try {
+        const response = await fetch(apiUrl);
+        const data = await response.json();
+        if(data.setup){
+            joke = `${data.setup} ... ${data.delivery}`;
+        }else{
+            joke = data.joke;
+        }
+        console.log(joke);
+    } catch (error) {
+        console.log('fetch error',error);
+    }
 }
 
-test();
+getJokes();
